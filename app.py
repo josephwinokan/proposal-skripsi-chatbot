d29145736f4fa5f00f83d65e863e2517b27c28e2
josephwinokan <jj441880@gmail.com>
2026-07-05 20:57:16 +0800

    Update FAQ dataset

--- app.py
+++ app.py
@@ -453,7 +453,9 @@ def chatbot():
 # 🚀 RUN APP
 # ================================
 if __name__ == "__main__":
+    import os
 
     app.run(
-        debug=True
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
     )
